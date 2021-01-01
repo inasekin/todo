16505acb7fad892261163e71c9694c9ca15618db
inasekin <nasekinid8591@yandex.ru>
2021-01-01 19:01:07 +0300

    test JSX syntax

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const TodoList = () => {
+    const items = ['Learn React', 'Build Awesome App', 'Drink coffee'];
     return (
         <ul>
-            <li>Learn React!</li>
-            <li>Build Awesome App</li>
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+            <li>{items[2]}</li>
         </ul>
     );
 };
@@ -17,14 +19,30 @@ const AppHeader = () => {
 };
 
 const SearchPanel = () => {
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px'
+    };
+
     return (
-        <input placeholder="search"/>
+        <input placeholder={ searchText }
+        className="foo"
+        style={ searchStyle }/>
     );
 };
 
 const App = () => {
+
+    const isLoggedIn = true;
+    const loginBox = <span>Log in please <br/></span>
+    const welcomeBox = <span>Welcome Back! 🤡<br/></span>
+
+    const value = '<script>alert("")</script>';
     return (
         <div>
+            { isLoggedIn ? welcomeBox : loginBox }
+            <span>{(new Date()).toString()}</span>
+            { value }
             <AppHeader />
             <SearchPanel />
             <TodoList />
