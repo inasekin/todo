5c39e01b74d33695ff92b551268b424a206a73ee
inasekin <nasekinid8591@yandex.ru>
2021-01-01 19:51:20 +0300

    add props

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,13 +3,14 @@ import React from 'react';
 import TodoListItem from './todo-list-item'
 
 const TodoList = () => {
-    const items = ['Learn React', 'Build Awesome App', 'Drink coffee'];
+    const items = ['Learn React', 'Build Awesome App'];
     return (
         <ul>
             <li>{items[0]}</li>
             <li>{items[1]}</li>
-            <li>{items[2]}</li>
-            <li><TodoListItem /></li>
+            <li><TodoListItem label="Drink coffee"/></li>
+            <li><TodoListItem label="Eat chiken" important={true}/></li>
+            <li><TodoListItem label="Build React App" /></li>
         </ul>
     );
 };
